094edef6629ec0be475ff5e2233d0f3c87044f9b
afreisthler <andrew.freisthler@proquest.com>
2018-05-11 13:27:49 +0300

    add led change

--- test.py
+++ test.py
@@ -6,5 +6,6 @@ import time
 button = ev3.Button()
 while True:
     if button.up:
+        ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.RED)
         ev3.Sound.speak('Hello World!').wait()
         time.sleep(2)
