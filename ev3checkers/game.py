dfee9a438d77a396888a9e05e59435b597490260
afreisthler <andrew.freisthler@proquest.com>
2018-05-30 10:37:42 +0300

    led update

--- ev3checkers/game.py
+++ ev3checkers/game.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import logging
 import ev3dev.ev3 as ev3
 from ev3checkers.components.board import Board
 
@@ -18,16 +17,10 @@ class Game(object):
 
 # This is the entry point for a call from skynet, launched through scheduler and func
 if __name__ == "__main__":
-    try:
-        ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.GREEN)
-        ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.GREEN)
-        game = Game()
-        game.draw()
-        ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.RED)
-        ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.RED)
-    except:
-        while True:
-            pass
+    ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.GREEN)
+    ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.GREEN)
+    game = Game()
+    game.draw()
 
     while True:
         pass
