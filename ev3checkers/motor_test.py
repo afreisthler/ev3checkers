270adc3dd28c3ea97be50fee29c99f4604a4c810
Andrew Freisthler <afreisthler@gmail.com>
2018-06-01 08:02:17 +0000

    motor test

--- ev3checkers/motor_test.py
+++ ev3checkers/motor_test.py
@@ -7,7 +7,14 @@ from time import sleep
 m1 = LargeMotor('outA')
 m2 = LargeMotor('outB')
 
-m1.run_to_rel_pos(position_sp=180, speed_sp=100, stop_action="hold")
-m2.run_to_rel_pos(position_sp=180, speed_sp=100, stop_action="hold")
+for i in range(7):
+    m1.run_to_rel_pos(position_sp=-110, speed_sp=150, stop_action="hold")
+    m2.run_to_rel_pos(position_sp=-110, speed_sp=150, stop_action="hold")
+    sleep(1)
 
-sleep(5)   # Give the motor time to move
+for i in range(7):
+    m1.run_to_rel_pos(position_sp=110, speed_sp=150, stop_action="hold")
+    m2.run_to_rel_pos(position_sp=110, speed_sp=150, stop_action="hold")
+    sleep(1)   
+
+
