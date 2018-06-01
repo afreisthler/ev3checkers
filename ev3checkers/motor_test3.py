4e4ae9bd81bb977ce5af87546e588fc4ab24d0d6
afreisthler <andrew.freisthler@proquest.com>
2018-06-01 15:14:27 +0300

    motor test 3

---
+++ ev3checkers/motor_test3.py
@@ -0,0 +1,39 @@
+
+from ev3dev.ev3 import *
+from time import sleep
+
+m1 = MediumMotor('outC')
+
+BLOCK_ROTATIONS = 135
+
+for i in range(7):
+    m1.run_to_rel_pos(position_sp=-BLOCK_ROTATIONS, speed_sp=150, stop_action="hold")
+    sleep(1)
+
+for i in range(7):
+    m1.run_to_rel_pos(position_sp=BLOCK_ROTATIONS, speed_sp=150, stop_action="hold")
+    sleep(1)
+
+m1 = LargeMotor('outA')
+m2 = LargeMotor('outB')
+
+BLOCK_ROTATIONS = 135
+direction = -1
+
+for i in range(7):
+    for i in range(7):
+        m1.run_to_rel_pos(position_sp=-BLOCK_ROTATIONS, speed_sp=150, stop_action="hold")
+        sleep(1)
+    direction = direction * -1
+    m1.run_to_rel_pos(position_sp=-110, speed_sp=150, stop_action="hold")
+    m2.run_to_rel_pos(position_sp=-110, speed_sp=150, stop_action="hold")
+    sleep(1)
+
+for i in range(7):
+    m1.run_to_rel_pos(position_sp=-BLOCK_ROTATIONS, speed_sp=150, stop_action="hold")
+    sleep(1)
+
+for i in range(7):
+    m1.run_to_rel_pos(position_sp=110, speed_sp=150, stop_action="hold")
+    m2.run_to_rel_pos(position_sp=110, speed_sp=150, stop_action="hold")
+    sleep(1)
